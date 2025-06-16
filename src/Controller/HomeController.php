cd747d16549f6c5c25e8d4ef4224a4712aa0bd4a
florian0503 <119812531+florian0503@users.noreply.github.com>
2025-06-16 22:29:23 +0200

    Revert "footer"

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,16 +12,4 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
-
-    #[Route('/mentions-legales', name: 'mentions_legales')]
-    public function mentionsLegales(): Response
-    {
-        return $this->render('legal/mentions_legales.html.twig');
-    }
-
-    #[Route('/tous-droits-reserves', name: 'tous_droits_reserves')]
-    public function tousDroitsReserves(): Response
-    {
-        return $this->render('legal/tous_droits_reserves.html.twig');
-    }
 }
