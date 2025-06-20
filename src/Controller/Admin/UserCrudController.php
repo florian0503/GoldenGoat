961a12399fe9a6c13c9d561ba6b65f3a6466d446
florian <florian@diagram.fr>
2025-06-20 12:02:33 +0200

    Ajout coté admin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,24 @@
+<?php
+
+// src/Controller/Admin/UserCrudController.php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield IdField::new('id')->onlyOnIndex();
+        yield TextField::new('email');
+        yield TextField::new('username');
+        // etc.
+    }
+}
