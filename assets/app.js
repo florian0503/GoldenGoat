a7c8635796f0d45f3b23523f44b5db68725069e2
florian0503 <florian.dimbert@next-u.fr>
2025-06-22 21:09:15 +0200

    correctif coté admin , probleme sass

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,6 @@
 import './styles/app.sass';
+
+import './styles/app.scss';
+
+import '../../vendor/easycorp/easyadmin-bundle/assets/js/app.js';
+import '../../vendor/easycorp/easyadmin-bundle/assets/styles/easyadmin.scss';
--- public/build/app.js
+++ public/build/app.js
@@ -9,6 +9,12 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_app_sass__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/app.sass */ "./assets/styles/app.sass");
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './styles/app.scss'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../../vendor/easycorp/easyadmin-bundle/assets/js/app.js'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../../vendor/easycorp/easyadmin-bundle/assets/styles/easyadmin.scss'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+
 
 
 /***/ }),
@@ -31,4 +37,4 @@ __webpack_require__.r(__webpack_exports__);
 /******/ var __webpack_exports__ = (__webpack_exec__("./assets/app.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUNBQSIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9hcHAuanMiLCJ3ZWJwYWNrOi8vLy4vYXNzZXRzL3N0eWxlcy9hcHAuc2Fzcz85MTgxIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9zdHlsZXMvYXBwLnNhc3MnOyIsIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQTJCO0FBRUE7QUFFc0M7Ozs7Ozs7Ozs7OztBQ0pqRSIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9hcHAuanMiLCJ3ZWJwYWNrOi8vLy4vYXNzZXRzL3N0eWxlcy9hcHAuc2Fzcz85MTgxIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9zdHlsZXMvYXBwLnNhc3MnO1xyXG5cclxuaW1wb3J0ICcuL3N0eWxlcy9hcHAuc2Nzcyc7XHJcblxyXG5pbXBvcnQgJy4uLy4uL3ZlbmRvci9lYXN5Y29ycC9lYXN5YWRtaW4tYnVuZGxlL2Fzc2V0cy9qcy9hcHAuanMnO1xyXG5pbXBvcnQgJy4uLy4uL3ZlbmRvci9lYXN5Y29ycC9lYXN5YWRtaW4tYnVuZGxlL2Fzc2V0cy9zdHlsZXMvZWFzeWFkbWluLnNjc3MnO1xyXG4iLCIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
