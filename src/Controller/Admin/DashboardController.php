7610bd2aa694b556d9e5aed1d5be38a2baa319dd
florian <florian@diagram.fr>
2025-06-23 14:05:00 +0200

    Ajout de l'authentification admin et des fixtures d'utilisateur

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,24 +1,14 @@
 <?php
+// src/Controller/Admin/DashboardController.php
 
 namespace App\Controller\Admin;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
-    public function index(): Response
-    {
-        return $this->render('admin/dashboard.html.twig', [
-            'dashboard_title' => 'Bienvenue sur le back-office !',
-        ]);
-    }
-
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -27,8 +17,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-
-        // tes entités CRUD, par exemple :
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // autres items…
     }
 }
